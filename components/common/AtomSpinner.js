dcd6e006f3f8d613ad4f8d8bb5014bf690782561
Arm Supakorn <rainstorm.armiie@gmail.com>
2018-11-14 12:23:17 +0700

    [⚡️] improve performance with css will-change

--- components/common/AtomSpinner.js
+++ components/common/AtomSpinner.js
@@ -53,36 +53,47 @@ const SpinnerLine = styled.div`
   border-right-style: solid;
   border-top-style: solid;
   border-top-color: transparent;
-  will-change: transform;
 
   &:nth-child(1) {
+    will-change: transform;
     animation: atom-spinner-animation-1 1s linear infinite;
+    -webkit-animation: atom-spinner-animation-1 1s linear infinite;
     transform: rotateZ(120deg) rotateX(66deg) rotateZ(0deg);
+    -webkit-transform: rotateZ(120deg) rotateX(66deg) rotateZ(0deg);
   }
   &:nth-child(2) {
+    will-change: transform;
     animation: atom-spinner-animation-2 1s linear infinite;
+    -webkit-animation: atom-spinner-animation-2 1s linear infinite;
     transform: rotateZ(240deg) rotateX(66deg) rotateZ(0deg);
+    -webkit-transform: rotateZ(240deg) rotateX(66deg) rotateZ(0deg);
   }
   &:nth-child(3) {
+    will-change: transform;
     animation: atom-spinner-animation-3 1s linear infinite;
+    -webkit-animation: atom-spinner-animation-3 1s linear infinite;
     transform: rotateZ(360deg) rotateX(66deg) rotateZ(0deg);
+    -webkit-transform: rotateZ(360deg) rotateX(66deg) rotateZ(0deg);
   }
 
   @keyframes atom-spinner-animation-1 {
     100% {
       transform: rotateZ(120deg) rotateX(66deg) rotateZ(360deg);
+      -webkit-transform: rotateZ(120deg) rotateX(66deg) rotateZ(360deg);
     }
   }
 
   @keyframes atom-spinner-animation-2 {
     100% {
       transform: rotateZ(240deg) rotateX(66deg) rotateZ(360deg);
+      -webkit-transform: rotateZ(240deg) rotateX(66deg) rotateZ(360deg);
     }
   }
 
   @keyframes atom-spinner-animation-3 {
     100% {
       transform: rotateZ(360deg) rotateX(66deg) rotateZ(360deg);
+      -webkit-transform: rotateZ(360deg) rotateX(66deg) rotateZ(360deg);
     }
   }
 `
@@ -100,7 +111,7 @@ const SpinnerCircle = styled.div`
   will-change: transform;
 `
 
-const spring = { ...config.default, precision: 0.1, duration: 800 }
+const spring = { ...config.default, precision: 0.1, duration: 500 }
 
 const AtomSpiner = ({ loading }) => {
   return (

f2ffc55fa856293c113b1d70b195a22fb445ff53
Arm Supakorn <rainstorm.armiie@gmail.com>
2018-11-14 23:09:56 +0700

    [🎨] improve structure for styled-component

--- components/head.js
+++ components/head.js
@@ -7,13 +7,16 @@ const defaultOGImage = ''
 
 const Head = props => (
   <NextHead>
-    {/* <meta charSet="UTF-8" /> */}
     <title>{props.title || ''}</title>
     <meta
       name="description"
       content={props.description || defaultDescription}
     />
-    {/* <meta name="viewport" content="width=device-width, initial-scale=1" /> */}
+    <meta charSet="UTF-8" />
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1, shrink-to-fit=no, viewport-fit=cover"
+    />
     <link rel="icon" sizes="192x192" href="/static/touch-icon.png" />
     <link rel="apple-touch-icon" href="/static/touch-icon.png" />
     <link rel="mask-icon" href="/static/favicon-mask.svg" color="#49B882" />

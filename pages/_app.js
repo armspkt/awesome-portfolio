968353c9918060a3577c9553e2effd12489b228d
Arm Supakorn <rainstorm.armiie@gmail.com>
2018-11-21 17:01:52 +0700

    🐛 fix: animation flashing and jumping

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import App, { Container } from 'next/app'
 import { Transition, animated } from 'react-spring'
 import AtomSpinner from '../components/common/AtomSpinner'
@@ -7,13 +7,6 @@ console.time('timer')
 export default class MyApp extends App {
   state = {
     loading: true
-  }
-
-  componentDidMount() {
-    setTimeout(() => {
-      this.setState({ loading: false })
-      console.timeEnd('timer')
-    }, 800)
   }
 
   static async getInitialProps({ Component, router, ctx }) {
@@ -26,43 +19,52 @@ export default class MyApp extends App {
     return { pageProps }
   }
 
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ loading: false })
+      console.timeEnd('timer')
+    }, 800)
+  }
+
   render() {
-    const { Component, pageProps } = this.props
+    const { Component, pageProps, router } = this.props
     const { loading } = this.state
     const items = [
       {
-        id: this.props.router.route,
-        Component: this.props.Component,
-        pageProps: this.props.pageProps
+        id: router.route,
+        Component: Component,
+        pageProps: pageProps
       }
     ]
 
     return (
       <Container>
-        <AtomSpinner loading={this.state.loading} />
-        {loading && <Component {...pageProps} />}
+        {loading && (
+          <Fragment>
+            <AtomSpinner loading={loading} />
+            <Component {...pageProps} />
+          </Fragment>
+        )}
 
         {!loading && (
-          <Transition
-            native
-            unique
-            items={items}
-            keys={items => items.id}
-            from={{
-              opacity: 0,
-              position: 'absolute',
-              width: '100%',
-              height: '100%'
-            }}
-            enter={{ opacity: 1 }}
-            leave={{ opacity: 0, position: 'absolute' }}
-          >
-            {({ Component, pageProps }) => styles => (
-              <animated.div style={styles}>
-                <Component {...pageProps} />
-              </animated.div>
-            )}
-          </Transition>
+          <div style={{ position: 'relative' }}>
+            <Transition
+              native
+              unique
+              items={items}
+              keys={items => items.id}
+              from={{ opacity: 0 }}
+              initial={{ opacity: 0 }}
+              enter={{ opacity: 1 }}
+              leave={{ opacity: 0, position: 'absolute' }}
+            >
+              {({ Component, pageProps }) => styles => (
+                <animated.div style={{ ...styles, width: '100%' }}>
+                  <Component {...pageProps} />
+                </animated.div>
+              )}
+            </Transition>
+          </div>
         )}
       </Container>
     )

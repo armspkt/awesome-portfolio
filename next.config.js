15940ac3b8ddcd5255ada711a4b0776178fa4b16
Arm Supakorn <rainstorm.armiie@gmail.com>
2018-12-03 11:46:01 +0700

    🔍 improve seo

--- next.config.js
+++ next.config.js
@@ -1,5 +1,21 @@
+const fs = require('fs')
+const { join } = require('path')
+const { promisify } = require('util')
+const copyFile = promisify(fs.copyFile)
+
 module.exports = {
-  webpack: (config) => {
+  exportPathMap: async function(
+    defaultPathMap,
+    { dev, dir, outDir, distDir, buildId }
+  ) {
+    if (dev) {
+      return defaultPathMap
+    }
+    // This will copy robots.txt from your project root into the out directory
+    await copyFile(join(dir, 'static/robots.txt'), join(outDir, 'robots.txt'))
+    return defaultPathMap
+  },
+  webpack: config => {
     // Fixes npm packages that depend on `fs` module
     config.node = {
       fs: 'empty'
--- pages/_document.js
+++ pages/_document.js
@@ -20,7 +20,7 @@ export default class MyDocument extends Document {
 
   render() {
     return (
-      <html>
+      <html lang="en">
         <Head>
           <meta charSet="utf-8" />
           <meta

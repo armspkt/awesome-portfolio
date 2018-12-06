7fec6531f72a151ee797e54f045476a941e136fd
Arm Supakorn <rainstorm.armiie@gmail.com>
2018-12-06 19:50:45 +0700

    🔍 improve seo

--- components/head.js
+++ components/head.js
@@ -2,8 +2,8 @@ import NextHead from 'next/head'
 import { string } from 'prop-types'
 
 const defaultTitle = `Arm Supakorn`
-const defaultDescription = `rainstormza's portfolio`
-const defaultOGURL = 'http://armspkt.firebaseapp.com'
+const defaultDescription = `Supakorn's awesome portfolio that used Nextjs to create. Usage React-Spring to create awesome animation. Usage Reactjs`
+const defaultOGURL = 'http://armspkt.firebaseapp.com/'
 const defaultOGImage = 'https://armspkt.firebaseapp.com/static/arm.jpg'
 
 const Head = props => (
@@ -18,28 +18,34 @@ const Head = props => (
       content="rainstormza, Supakorn Thongtra, Arm, Portfolio, React, Nextjs, styled-components"
     />
     <meta name="author" content="Supakorn Thongtra" />
-    {/* <meta charSet="utf-8" />
-    <meta
-      name="viewport"
-      content="width=device-width, initial-scale=1, shrink-to-fit=no, viewport-fit=cover"
-    /> */}
     <link rel="icon" sizes="192x192" href="/static/touch-icon.png" />
     <link rel="apple-touch-icon" href="/static/touch-icon.png" />
     <link rel="mask-icon" href="/static/favicon-mask.svg" color="#49B882" />
     <link rel="icon" href="/static/favicon.ico" />
-    <meta property="og:url" content={props.url || defaultOGURL} />
+
+    <meta property="fb:app_id" content="1980447875380995" />
+    <meta property="og:site_name" content="Arm Supakorn" />
     <meta property="og:title" content={props.title || defaultTitle} />
     <meta
       property="og:description"
       content={props.description || defaultDescription}
     />
-    <meta name="twitter:site" content={props.url || defaultOGURL} />
-    <meta name="twitter:card" content="summary_large_image" />
-    <meta name="twitter:image" content={props.ogImage || defaultOGImage} />
+    <meta property="og:type" content="website" />
+    <meta property="og:url" content={props.url || defaultOGURL} />
     <meta property="og:image" content={props.ogImage || defaultOGImage} />
     <meta property="og:image:width" content="1200" />
     <meta property="og:image:height" content="630" />
     <meta property="og:image:alt" content="Arm is smiling." />
+    <meta property="og:locale" content="en_US" />
+
+    <meta name="twitter:title" content={props.title || defaultTitle} />
+    <meta
+      name="twitter:description"
+      content={props.description || defaultDescription}
+    />
+    <meta name="twitter:image" content={props.ogImage || defaultOGImage} />
+    <meta name="twitter:site" content="@armspkt" />
+    <meta name="twitter:card" content="summary_large_image" />
   </NextHead>
 )
 
--- pages/_document.js
+++ pages/_document.js
@@ -27,6 +27,11 @@ export default class MyDocument extends Document {
             name="viewport"
             content="width=device-width, initial-scale=1, shrink-to-fit=no, viewport-fit=cover"
           />
+          <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+          <meta
+            name="google-site-verification"
+            content="tSFXXxeK1C4D-BZuHfJSc0nbyPqDTB7U60rXqIn9MOE"
+          />
         </Head>
         <body>
           <Main />

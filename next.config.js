3554d52aa42f76fa584d9e9137f1f539d6310c8d
Arm Supakorn <rainstorm.armiie@gmail.com>
2018-12-06 19:52:57 +0700

    ✨ add polyfills for chrome41

--- next.config.js
+++ next.config.js
@@ -21,6 +21,20 @@ module.exports = {
       fs: 'empty'
     }
 
+    const originalEntry = config.entry
+    config.entry = async () => {
+      const entries = await originalEntry()
+
+      if (
+        entries['main.js'] &&
+        !entries['main.js'].includes('./polyfills.js')
+      ) {
+        entries['main.js'].unshift('./polyfills.js')
+      }
+
+      return entries
+    }
+
     return config
   }
 }

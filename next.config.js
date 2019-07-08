e36159f0e0084b49ad5eff431a2ea3efd157c85f
armm <armm.supakorn@gmail.com>
2019-07-09 03:32:58 +0700

    ⬆️ update nextjs9 + pure typescript

--- next.config.js
+++ next.config.js
@@ -3,53 +3,31 @@ const { join } = require('path')
 const { promisify } = require('util')
 const copyFile = promisify(fs.copyFile)
 const withOffline = require('next-offline')
-const withTypescript = require('@zeit/next-typescript')
 
-module.exports = withTypescript(
-  withOffline({
-    exportPathMap: async function(
-      defaultPathMap,
-      { dev, dir, outDir, distDir, buildId }
-    ) {
-      if (dev) {
-        return defaultPathMap
-      }
-      // This will copy robots.txt from your project root into the out directory
-      await copyFile(join(dir, 'static/robots.txt'), join(outDir, 'robots.txt'))
-      await copyFile(
-        join(dir, 'static/manifest.json'),
-        join(outDir, 'manifest.json')
-      )
-      await copyFile(
-        join(dir, 'static/sitemap.xml'),
-        join(outDir, 'sitemap.xml')
-      )
+module.exports = withOffline({
+  exportPathMap: async function(
+    defaultPathMap,
+    { dev, dir, outDir, distDir, buildId }
+  ) {
+    if (dev) {
       return defaultPathMap
-    },
-    webpack: config => {
-      // Fixes npm packages that depend on `fs` module
-      config.node = {
-        fs: 'empty'
-      }
-
-      // const originalEntry = config.entry
-      // config.entry = async () => {
-      //   const entries = await originalEntry()
-
-      //   if (
-      //     entries['main.js'] &&
-      //     !entries['main.js'].includes('./polyfills.js')
-      //   ) {
-      //     entries['main.js'].unshift('./polyfills.js')
-      //   }
-
-      //   return entries
-      // }
-
-      return config
     }
-  })
-)
+    // This will copy robots.txt from your project root into the out directory
+    await copyFile(join(dir, 'static/robots.txt'), join(outDir, 'robots.txt'))
+    await copyFile(
+      join(dir, 'static/manifest.json'),
+      join(outDir, 'manifest.json')
+    )
+    await copyFile(join(dir, 'static/sitemap.xml'), join(outDir, 'sitemap.xml'))
+    return defaultPathMap
+  },
+  webpack: config => {
+    // Fixes npm packages that depend on `fs` module
+    config.node = {
+      fs: 'empty'
+    }
+    return config
+  }
+})
 
-// https://github.com/zeit/next-plugins/tree/master/packages/next-typescript
 // https://github.com/hanford/next-offline

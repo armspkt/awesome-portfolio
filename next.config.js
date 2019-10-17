2844c239960326029dd9cb785e35d709721b621c
Supakorn Thongtra <armm.supakorn@gmail.com>
2019-10-18 02:15:53 +0700

    🔧 config webpack analyzer

--- next.config.js
+++ next.config.js
@@ -1,16 +1,36 @@
 const withOffline = require('next-offline')
+const withBundleAnalyzer = require('@zeit/next-bundle-analyzer')
 
-module.exports = withOffline({
-  // exportTrailingSlash: true,
-  webpack: (config, { isServer }) => {
-    // Fixes npm packages that depend on `fs` module
-    if (!isServer) {
-      config.node = {
-        fs: 'empty'
+module.exports = withBundleAnalyzer(
+  withOffline({
+    // exportTrailingSlash: true,
+    // next-offline
+    workboxOpts: {
+      globPatterns: ['/public/**/*']
+    },
+    // next-bundle-analyzer
+    analyzeServer: ['server', 'both'].includes(process.env.BUNDLE_ANALYZE),
+    analyzeBrowser: ['browser', 'both'].includes(process.env.BUNDLE_ANALYZE),
+    bundleAnalyzerConfig: {
+      server: {
+        analyzerMode: 'static',
+        reportFilename: '../bundles/server.html'
+      },
+      browser: {
+        analyzerMode: 'static',
+        reportFilename: '../bundles/client.html'
       }
+    },
+    webpack: (config, { isServer }) => {
+      // Fixes npm packages that depend on `fs` module
+      if (!isServer) {
+        config.node = {
+          fs: 'empty'
+        }
+      }
+      return config
     }
-    return config
-  }
-})
+  })
+)
 
 // https://github.com/hanford/next-offline
